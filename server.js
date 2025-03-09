7f47263ddb915a49928aa108e7371ed3385b5b62
David Y. Stephenson <david@davidystephenson.com>
2025-03-09 11:48:28 -0400

    People queries

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const http = require('http')
 const url = require('url')
 
+const people = [
+  'Dorothy',
+  'Tallullah'
+]
+
 const server = http.createServer((request, response) => {
-  const parts = url.parse(request.url)
-  console.log('parts', parts)
+  const parts = url.parse(request.url, true)
   switch (parts.pathname) {
     case '/hello': {
       return response.end('hello!!!')
     }
     case '/goodbye': {
       return response.end('goodbye???')
+    }
+    case '/people': {
+      if (parts.query.index) {
+        const person = people[parts.query.index]
+        if (!person) {
+          const message = {
+            message: 'Person not found'
+          }
+          const json = JSON.stringify(message)
+          return response.end(json)
+        }
+        const json = JSON.stringify({ person })
+        return response.end(json)
+      }
+      const json = JSON.stringify(people)
+      return response.end(json)
+    }
+    default: {
+      const message = {
+        message: 'Not found'
+      }
+      const json = JSON.stringify(message)
+      return response.end(json)
     }
   }
 })
